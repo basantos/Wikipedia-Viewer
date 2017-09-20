ddaa1baa085aac58d0cd0e1fc4045269779ccd2c
basantos <santosbrigitte@gmail.com>
2017-09-19 17:20:09 -0700

    Add random and search wiki mechanisms and basic css

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function retrieveWikis(){
+  var keyword = document.getElementById("search").value;
+  $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" + keyword + "&callback=?", function(data){
+    var html = "";
+    var itle = "";
+    var summary = "";
+    var link = "";
+    var linkHeader = "";
+    for (var i = 0; i < data.query.search.length; i++){
+      title = data.query.search[i].title;
+      summary = data.query.search[i].snippet;
+      linkHeader = title.replace(/[" "]/g,"_");
+      link = "https://en.m.wikipedia.org/wiki/" + linkHeader;
+      html += "<br><a href=" + link + " target='_blank'><section class='entry'><h1>" + title + "</h1><br>" + summary + "</section></a>"
+    }
+    $(".display").html(html);
+    //$(".display").html(JSON.stringify(data.query.search[1].title));
+  });
+}
+
+$(document).ready(function(){
+  $("#submit").click(function(){
+    retrieveWikis();
+  });
+  $("#search").keypress(function(e){
+    if (e.keyCode == 13){
+      event.preventDefault();
+      retrieveWikis();
+    }
+  })
+})
